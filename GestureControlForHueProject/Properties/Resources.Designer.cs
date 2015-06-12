2cec8b8d193e07170ff1cea8a3925fb5cd082f29
sabinewinkler <sabine.winkler@students.fh-hagenberg.at>
2015-06-12 13:44:33 +0200

    Renamed the ui project from 'MUS2' to 'MUS2.UI' and adapted the namespace.

--- GestureControlForHueProject/Properties/Resources.Designer.cs
+++ GestureControlForHueProject/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MUS2.Properties {
+namespace MUS2.UI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MUS2.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MUS2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MUS2.UI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- GestureControlForHueProject/Properties/Settings.Designer.cs
+++ GestureControlForHueProject/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MUS2.Properties {
+namespace MUS2.UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
