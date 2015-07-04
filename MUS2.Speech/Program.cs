416fe36273e55aa523c5f8eb9b8b35476893f913
sabinewinkler <sabine.winkler@students.fh-hagenberg.at>
2015-07-04 22:27:47 +0200

    Added class comment and removed unused usings.

--- MUS2.Speech/Program.cs
+++ MUS2.Speech/Program.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Speech.Recognition;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading;
 
 namespace MUS2.Speech {
-  class Program {
-    static ManualResetEvent _completed = null;
 
-    static void Main(string[] args) {
+  //
+  // Summary:
+  //     Console application to initialize and start the speech recognition.
+  //
+  // Authors:
+  //     Florentina Grebe
+  //     Sabine Winkler
+  //
+  // Since:
+  //     2015-07-08
+  //
+  public class Program {
+
+    private static ManualResetEvent _completed = null;
+
+    public static void Main(string[] args) {
       _completed = new ManualResetEvent(false);
 
       SpeechRecognition rec = new SpeechRecognition();
-      rec.EnableSpeech(); // enables recognition and loads grammar file
+      rec.EnableSpeech();   // enables recognition and loads grammar file
       _completed.WaitOne(); // wait until speech recognition is completed
     }
   }
